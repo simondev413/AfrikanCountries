938be525049d623c5442218d2354d4c18d00c309
simondev413 <devemail413@gmail.com>
2025-11-22 17:58:29 +0100

    created the country schema.

---
+++ src/entities/Country.ts
@@ -0,0 +1,37 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import "reflect-metadata";
+
+@Entity("countries")
+export class Country {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  capital!: string;
+
+  @Column()
+  population!: number;
+
+  @Column()
+  area!: number;
+
+  @Column()
+  isLandlocked!: boolean;
+
+  @Column("text", { array: true })
+  languages!: string[];
+
+  @Column()
+  currency!: string | null;
+
+  @Column("decimal", { precision: 10, scale: 6 })
+  latitude!: number;
+
+  @Column("decimal", { precision: 10, scale: 6 })
+  longitude!: number;
+}
+
+export default Country;
