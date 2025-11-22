11a5c1a3f6bbb2a68c884a50c02fcaffed93dd16
simondev413 <devemail413@gmail.com>
2025-11-22 17:57:30 +0100

    created some utils functions.

---
+++ src/utils/helpers.ts
@@ -0,0 +1,15 @@
+import { Country } from "../entities/Country";
+
+export function calculatePopulationDensity(country: Country): number {
+    return country.population / country.area;
+}   
+
+export function getMostPopulousCountry(countries: Country[]): Country | null {
+    if (countries.length === 0) return null;    
+    return countries.reduce((max, country) => country.population > max.population ? country : max, countries[0]);
+}
+
+export function getLargestCountryByArea(countries: Country[]): Country | null {
+    if (countries.length === 0) return null;    
+    return countries.reduce((max, country) => country.area > max.area ? country : max, countries[0]);
+}
