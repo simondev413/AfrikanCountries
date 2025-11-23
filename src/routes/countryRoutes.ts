684eb4bc46c59503a06eec749c7f962768b4136b
simondev413 <devemail413@gmail.com>
2025-11-23 19:27:06 +0100

    added api version to the routes

--- src/routes/countryRoutes.ts
+++ src/routes/countryRoutes.ts
@@ -10,10 +10,11 @@ import {
 //Criação do roteador para as rotas de país
 const router = Router();
 
-router.get("/countries", getAllCountries); //Rota para obter todos os países
-router.get("/countries/:id", getCountryById);//Rota para obter um país por ID
-router.post("/countries", createCountry); //Rota para criar um novo país
-router.put("/countries/:id", updateCountry); //Rota para atualizar um país existente
-router.delete("/countries/:id", deleteCountry); //Rota para remover um país (opcional  )
+router.get("/api/v1/countries", getAllCountries); //Rota para obter todos os países
+router.get("/api/v1/countries/:id", getCountryById);//Rota para obter um país por ID
+
+router.post("/api/v1/countries", createCountry); //Rota para criar um novo país
+router.put("/api/v1/countries/:id", updateCountry); //Rota para atualizar um país existente
+router.delete("/api/v1/countries/:id", deleteCountry); //Rota para remover um país (opcional  )
 
 export default router;
