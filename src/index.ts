0e82b431978117f851af21c41e4df49e3fd674f0
simondev413 <devemail413@gmail.com>
2025-11-23 19:21:07 +0100

    changed the main server adress

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,25 @@
 import app from "./app";
 import { AppDataSource } from "./database/data-source";
 import * as dotenv from "dotenv";
+
 dotenv.config();
 
-console.log(process.env.DB_NAME);
 //Definição da porta do servidor
 const PORT = process.env.PORT || 3001; //Definição da porta do servidor
 
 //Inicialização da fonte de dados
 AppDataSource.initialize()
-    .then(() => {
-        //Fonte de dados inicializada com sucesso
-        console.log("Data Source has been initialized!");
-    })
-    .catch((err) => {
-        //Erro durante a inicialização da fonte de dados
-        console.error("Error during Data Source initialization:", err);
-    });
+  .then(() => {
+      console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    //Erro durante a inicialização da fonte de dados
+    console.error("Error during Data Source initialization:", err);
+  });
 
 //Início do servidor
-function onListen(){
-    console.log(`Server is running on http://localhost:${PORT}`);
+function onListen() {
+  console.log(`Server is running on http://localhost:${PORT}/api/v1`);
 }
 
-
-app.listen(PORT, onListen); //Início do servidor na porta definida 
+app.listen(PORT, onListen); //Início do servidor na porta definida
