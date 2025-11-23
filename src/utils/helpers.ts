553888bee5bc3fbdf410ff32046c997f24f08380
simondev413 <devemail413@gmail.com>
2025-11-23 19:27:55 +0100

    created function to verify hashed passwords

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -13,3 +13,11 @@ export function getLargestCountryByArea(countries: Country[]): Country | null {
     if (countries.length === 0) return null;    
     return countries.reduce((max, country) => country.area > max.area ? country : max, countries[0]);
 }
+
+ export function isBcryptHash(password: string) {
+      return (
+        password.startsWith("$2a$") ||
+        password.startsWith("$2b$") ||
+        password.startsWith("$2y$")
+      );
+    }
