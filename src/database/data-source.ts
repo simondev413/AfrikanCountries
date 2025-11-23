f71e538e4d93d63c6e6dfe05dc266f5f4cfaaaf2
simondev413 <devemail413@gmail.com>
2025-11-23 19:23:55 +0100

    set synchronize and dropschema true

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,6 +3,8 @@ import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
 
 import { Country } from "../entities/Country";
+import User from "../entities/User"
+import { AuditLog } from "../entities/Audit";
 
 dotenv.config();
 
@@ -10,12 +12,14 @@ dotenv.config();
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3306,  
+    port: Number(process.env.DB_PORT),  
     username: process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: true, 
     logging: true,
-    entities: [Country],
+    entities: [User, Country,AuditLog],
     driver: require('mysql2'),
+    synchronize: true,
+    dropSchema: true, 
+
 })
